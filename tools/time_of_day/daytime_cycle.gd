d161c3e1f9fc586e0a353f3705b3a9b18263b3f9
Torben Van Assche <torbenvanassche.anime@gmail.com>
2026-03-19 00:08:38 +0100

    Update daytime_cycle.gd

--- tools/time_of_day/daytime_cycle.gd
+++ tools/time_of_day/daytime_cycle.gd
@@ -2,3 +2,13 @@ class_name TimeOfDay extends Node
 
 ##Day duration in seconds
 @export var duration: int = 100;
+
+@onready var day_timer: Timer = Timer.new();
+
+signal day_ended();
+
+func _ready() -> void:
+	day_timer.wait_time = duration
+	day_timer.timeout.connect(day_ended.emit);
+	day_timer.autostart = true;
+	add_child(day_timer)
