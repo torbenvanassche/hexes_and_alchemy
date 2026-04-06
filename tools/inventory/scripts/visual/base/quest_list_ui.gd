082931ce1fe9c70ce0c85c470a2fac7a6436cdd8
Torben Van Assche <torbenvanassche.anime@gmail.com>
2026-04-07 00:15:02 +0200

    working on quest creation and management UI

---
+++ tools/inventory/scripts/visual/base/quest_list_ui.gd
@@ -0,0 +1,7 @@
+class_name QuestListUI extends VBoxContainer
+
+func set_content(quests: Array[Quest]) -> void:
+	pass
+
+func on_enable() -> void:
+	pass;
--- tools/quest_system/scripts/quest_board.gd
+++ tools/quest_system/scripts/quest_board.gd
@@ -1,7 +1,19 @@
 class_name QuestBoard extends Interaction
 
-func interact() -> void:
-	return
+var window_instance: SceneInstance;
 
+func interact() -> void:
+	DataManager.instance.get_scene_by_name("quest_list_ui").queue(_open_window)
+	
 func can_interact() -> bool:
-	return true;
+	return not window_instance || not SceneManager.is_visible(window_instance)
+	
+func _open_window(window_info: SceneInfo) -> void:
+	window_instance = SceneManager.add(window_info, false);
+	var quest_list_ui: QuestListUI = (window_instance.node as DraggableControl).content as QuestListUI;
+	window_instance.on_enter.emit();
+
+func _on_area_exit(other: Area3D) -> void:
+	if window_instance:
+		window_instance.hide();
+	super(other);
