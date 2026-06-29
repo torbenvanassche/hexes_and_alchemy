84e126de675650c916d272bdac44d45c1bad7a96
Torben Van Assche <torbenvanassche.anime@gmail.com>
2026-06-29 14:20:12 +0200

    fixed reference mismatches

--- scripts/helpers/debug.gd
+++ scripts/helpers/debug.gd
@@ -1,19 +1,24 @@
 extends Node
 
 var show_log: bool = true;
+var notify_runtime: bool = true;
 
 func message(s: String) -> void:
 	if show_log:
 		print("MESSAGE: %s" % [s]);
+	
+	if notify_runtime:
 		Manager.instance.toast.notify(s)
 		
 func warn(s: String) -> void:
 	if show_log:
 		push_warning(s);
 		print_rich("[color=yellow]WARNING: %s[/color]" % s)
+	if notify_runtime:
 		Manager.instance.toast.notify(s, Color.YELLOW)
 		
 func err(s: String) -> void:
 	if show_log:
 		printerr(s);
+	if notify_runtime:
 		Manager.instance.toast.notify(s, Color.RED)
