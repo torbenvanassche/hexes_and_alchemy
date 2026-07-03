a28e5aba0044402dbef8ac2171f2eea09acd6208
Torben Van Assche <torbenvanassche.anime@gmail.com>
2026-07-03 13:50:56 +0200

    added missing keybind for inventory

--- tools/input_prompts/scripts/input_manager.gd
+++ tools/input_prompts/scripts/input_manager.gd
@@ -17,6 +17,7 @@ extends Node
 	
 	"toggle_input": "KEYBIND_TOGGLE_INPUT",
 	"primary_action": "KEYBIND_PRIMARY_ACTION",
+	"inventory": "KEYBIND_INVENTORY",
 	"journal": "KEYBIND_JOURNAL",
 	
 	"cancel": "KEYBIND_CANCEL"
